e0db39060d192a8815cd66642dd77cea88882ba4
plastikSun <marcom2k01@gmail.com>
2024-02-08 14:31:47 +0100

    install: routers

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "./assets/scss/style.scss";
 import * as bootstrap from "bootstrap";
 import App from "./App.vue";
+import { router } from "./router/router.js";
 
-createApp(App).mount("#app");
+createApp(App).use(router).mount("#app");
