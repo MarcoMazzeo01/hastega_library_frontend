e2963c8520eeb9d7a28f7d495e94fb11f4dc2a11
plastikSun <marcom2k01@gmail.com>
2024-02-08 14:31:28 +0100

    add: router-links to library and users (test)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup>
+<script>
 import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  data() {
+    return {};
+  },
+
+  components: {
+    HelloWorld,
+  },
+};
 </script>
 
 <template>
   <HelloWorld msg="Hello" />
+  <p><router-link to="/library">library</router-link></p>
+  <router-link to="/">users</router-link>
+  <router-view></router-view>
 </template>
 
 <style lang="scss"></style>
