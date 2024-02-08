b7168d0ef1c4feed347797561f211a4becf8e3f9
plastikSun <marcom2k01@gmail.com>
2024-02-08 14:31:10 +0100

    create: library and users pages

---
+++ src/pages/library.vue
@@ -0,0 +1,21 @@
+<script>
+// import MyComponent from
+export default {
+  data() {
+    return {
+      title: "Libreria",
+    };
+  },
+
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  hihih
+</template>
+
+<style lang="scoped"></style>
---
+++ src/pages/users.vue
@@ -0,0 +1,22 @@
+<script>
+// import MyComponent from
+export default {
+  data() {
+    return {
+      title: "Login",
+    };
+  },
+
+  mounted() {},
+  // components: {
+  //   MyComponent,
+  // },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  test test testtt
+</template>
+
+<style lang="scoped"></style>
