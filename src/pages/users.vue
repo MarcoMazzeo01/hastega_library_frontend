c0ab1a5aa38eca8d6602b472c10fad9d24232f35
plastikSun <marcom2k01@gmail.com>
2024-02-08 17:17:21 +0100

    add: fetch users & library route (no api call yet)

--- src/pages/users.vue
+++ src/pages/users.vue
@@ -1,21 +1,37 @@
 <script>
-// import MyComponent from
+import axios from "axios";
+
 export default {
   data() {
     return {
       title: "Utenti",
+      users: "",
     };
   },
 
   // components: {
   //   MyComponent,
   // },
+
+  mounted() {
+    axios.get("http://127.0.0.1:8000/api/users").then((response) => {
+      this.users = response.data;
+      console.log(this.users);
+    });
+  },
 };
 </script>
 
 <template>
   <h1>{{ title }}</h1>
-  hihih
+
+  <ul v-for="user in this.users">
+    <li>
+      <router-link :to="{ name: 'library', params: { userId: user.id } }"
+        >{{ user.first_name }} {{ user.last_name }}</router-link
+      >
+    </li>
+  </ul>
 </template>
 
 <style lang="scoped"></style>
