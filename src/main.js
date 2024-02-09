bf032ea5ca956c3a133785ee44e1c8be08e397eb
plastikSun <marcom2k01@gmail.com>
2024-02-09 17:01:38 +0100

    install: vue-cookies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import "./assets/scss/style.scss";
 import * as bootstrap from "bootstrap";
-import App from "./App.vue";
+import VueCookies from "vue-cookies";
+import _App from "./App.vue";
 import { router } from "./router/router.js";
 
-createApp(App).use(router).mount("#app");
+const App = createApp(_App);
+
+App.use(router);
+App.provide("$cookies", VueCookies);
+App.mount("#app");
