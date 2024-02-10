0fdda279b88a612c4955652544379e6101266985
plastikSun <marcom2k01@gmail.com>
2024-02-10 05:02:02 +0100

    add: redirect route to book page after creation

--- src/pages/book_form.vue
+++ src/pages/book_form.vue
@@ -1,10 +1,36 @@
 <script>
+import axios from "axios";
+
 // import MyComponent from
 export default {
   data() {
     return {
       title: "Nuovo Libro",
+      formData: {
+        title: "",
+        author: "",
+        isbn: "",
+        plot: "",
+      },
     };
+  },
+
+  methods: {
+    async handleSubmit() {
+      axios
+        .post("http://127.0.0.1:8000/api/books/", this.formData)
+        .then((response) => {
+          console.log(response);
+          if (response.status === 200) {
+            const bookId = response.data.id;
+            //this.$router.push('book', )
+            this.$router.push({ path: `/book/${bookId}` });
+          }
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
   },
 
   // components: {
@@ -16,22 +42,46 @@ export default {
 <template>
   <h1>{{ title }}</h1>
 
-  <form action="http://127.0.0.1:8000/api/books/" method="POST">
+  <form @submit.prevent="handleSubmit">
     <!-- book title -->
     <label for="title" class="form-label">Titolo</label>
-    <input type="text" class="form-control" id="title" name="title" />
+    <input
+      v-model.lazy="this.formData.title"
+      type="text"
+      class="form-control"
+      id="title"
+      name="title"
+    />
 
     <!-- book author -->
     <label for="author" class="form-label">Autore</label>
-    <input type="text" class="form-control" id="title" name="author" />
+    <input
+      v-model.lazy="this.formData.author"
+      type="text"
+      class="form-control"
+      id="title"
+      name="author"
+    />
 
     <!-- book isbn -->
     <label for="isbn" class="form-label">Codice ISBN</label>
-    <input type="text" class="form-control" id="isbn" name="isbn" />
+    <input
+      v-model.lazy="this.formData.isbn"
+      type="text"
+      class="form-control"
+      id="isbn"
+      name="isbn"
+    />
 
     <!-- book plot -->
     <label for="plot" class="form-label">Trama</label>
-    <textarea class="form-control" id="plot" name="plot" rows="4"></textarea>
+    <textarea
+      v-model.lazy="this.formData.plot"
+      class="form-control"
+      id="plot"
+      name="plot"
+      rows="4"
+    ></textarea>
 
     <button type="submit" class="btn btn-primary">Salva</button>
   </form>
